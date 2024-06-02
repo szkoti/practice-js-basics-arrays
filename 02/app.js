38d85acd3163b7eeb9392842d761ed2049581a33
szkoti <120187555+szkoti@users.noreply.github.com>
2024-06-02 20:19:22 +0200

    array length and index

--- 02/app.js
+++ 02/app.js
@@ -1,6 +1,17 @@
 const randomArray = createRandomArray();
-console.log(randomArray);
+//console.log(randomArray);
 
+
+//     for (let i = 0; i < randomArray.length; i++) {
+//         console.log(randomArray[i]);
+// };
+
+randomArray.forEach(function(it, idx) {
+    console.log( it, idx);
+});
+
+const lastIndex = randomArray.length - 1;
+console.log(randomArray[lastIndex]);
 
 
 // nie modyfikuj kodu poniżej!
