7ce68dbbd560e682703cfc5823ebf562cc90c660
szkoti <120187555+szkoti@users.noreply.github.com>
2024-06-02 19:32:55 +0200

    array with names

---
+++ 01/app.js
@@ -0,0 +1,9 @@
+// W pliku `app.js` stwórz zmienną `users` i przypisz do niej tablicę, 
+// która będzie przechowywać imiona i nazwiska przedstawione w formie ciągu znaków.
+// Następnie wyświetl w konsoli informacje o pierwszym, trzecim i piątym użytkowniku.
+// Dodatkowo z pomocą właściwości `.length` wyświetl, ile elementów zawiera tablica.
+
+let users = ["John Lemon", "Igi Pop", "Lukas Papiez", "Krystian Metan", "Wanda Coniemcaniechciala", "Kunegunda Pinczak" ];
+
+console.log(users.length);
+console.log(users[0], users[2], users[4]);
