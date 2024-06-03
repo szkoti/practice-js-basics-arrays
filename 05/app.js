bfc058e36d37ddb45962e469066bd62064644864
szkoti <120187555+szkoti@users.noreply.github.com>
2024-06-03 11:27:12 +0200

    filter() & reduce()

--- 05/app.js
+++ 05/app.js
@@ -1,1 +1,14 @@
+// W pliku `app.js` znajdziesz zmienną o nazwie `numbers`, która przechowuje tablicę liczb.
+// Twoim zadaniem będzie obliczenie sumy wszystkich liczb parzystych znajdujących się w tablicy.
+// Do rozwiązania tego zadania wykorzystaj funkcję tablicową `.filter()` oraz `.reduce()`.
+
+
 const numbers = [1, 2, 3, 4, 5, 6, 7];
+
+const sumOfEvenNumbers = numbers.filter( function(el){
+    return el % 2 === 0;
+}).reduce(function(a, b) {
+    return a + b;
+});
+
+console.log(sumOfEvenNumbers);
